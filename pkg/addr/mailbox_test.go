4e9e5224a286d4f93a9bf470f59c9a6054017111
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-03-02 17:37:30 -0600

    add a test for mime word decoding

--- pkg/addr/mailbox_test.go
+++ pkg/addr/mailbox_test.go
@@ -22,3 +22,13 @@ func TestMailbox(t *testing.T) {
 	assert.Equal(t, "", mb.OriginalString())
 	assert.Equal(t, "\"Peyton Randalf\" <peyton.randalf@example.com> (Virginia House of Burgesses)", mb.CleanString())
 }
+
+func TestUnicodeMailbox(t *testing.T) {
+	mb, err := ParseEmailMailbox("=?utf-8?q?St=C3=A9rl=C3=AF=C3=B1g?= <sterling@example.com> (=?utf-8?q?=C2=A1Hola,_se=C3=B1or!?=)")
+	assert.NoError(t, err)
+
+	assert.Equal(t, "Stérlïñg", mb.DisplayName())
+	assert.Equal(t, "sterling@example.com", mb.Address())
+	assert.Equal(t, "¡Hola, señor!", mb.Comment())
+	assert.Equal(t, "=?utf-8?q?St=C3=A9rl=C3=AF=C3=B1g?= <sterling@example.com> (=?utf-8?q?=C2=A1Hola,_se=C3=B1or!?=)", mb.OriginalString())
+}
